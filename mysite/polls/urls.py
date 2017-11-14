b624b0348595d0c98d3a5a986d2a90cf885efba5
Jeremy Spencer <jeremytiki@gmail.com>
2017-11-14 16:48:00 -0500

    Basic upload for video 1.4

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -5,6 +5,8 @@ from . import views
 app_name = 'polls'
 urlpatterns = [
     url(r'^$', views.IndexView.as_view()),
-    url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view()),
     url(r'^(?P<pk>[0-9]+)/delete/$', views.DeleteView.as_view(), name='delete'),
+    url(r'^(?P<pk>[0-9]+)/vote/$', views.VoteView.as_view()),
+    url(r'^(?P<pk>[0-9]+)/results/$', views.ResultsView.as_view()),
+    url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view()),
 ]
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,7 +1,11 @@
 from django.views import generic
+from django.shortcuts import redirect
+from django.views.generic.base import TemplateResponseMixin
 
 from .models import Question
+from .models import Choice
 from .mixins import RequireLoginMixin
+
 
 """
 def index(request):
@@ -36,3 +40,35 @@ class DetailView(generic.DetailView):
 class DeleteView(generic.DeleteView):
     model = Question
     success_url = "/polls/"
+
+
+class VoteView(generic.View):
+
+    def get_queryset(self, question_id, choice_id):
+        question = Question.objects.get(pk=question_id)
+        return question.choice_set.get(pk=choice_id)
+
+    def post(self, request, pk):
+        question_id = pk
+        choice_id = request.POST.get('choice', None)
+        try:
+            queryset = self.get_queryset(question_id, choice_id)
+        except (KeyError, Choice.DoesNotExist):
+            return redirect('/polls/' + question_id)
+        else:
+            queryset.votes += 1
+            queryset.save()
+            return redirect('/polls/' + question_id + '/results/')
+
+
+class ResultsView(TemplateResponseMixin, generic.View):
+    template_name = 'polls/results.html'
+
+    def get_queryset(self, question_id):
+        return Question.objects.get(pk=question_id)
+
+    def get(self, request, pk):
+        print(pk)
+        queryset = self.get_queryset(pk)
+        context = {'question': queryset}
+        return self.render_to_response(context)
